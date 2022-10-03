9b6c1071952c8953fbbfa37f0225ffc001b39e5f
sultanajayi <hayobamidele39@gmail.com>
2022-10-03 21:59:30 +0100

    New Update

---
+++ Python Task/task_8.py
@@ -0,0 +1,13 @@
+number = 11
+
+for i in range (number):
+    
+    if i == 2:
+        number -=1
+        continue
+    print('Number', i)    
+    if i == 8:
+        break
+        
+         
+        
