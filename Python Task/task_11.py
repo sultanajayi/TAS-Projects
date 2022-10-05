f29bdbd7986f7c8fe4f55356308f02f08a05ce46
sultanajayi <hayobamidele39@gmail.com>
2022-10-05 13:25:41 +0100

    Latest Update

---
+++ Python Task/task_11.py
@@ -0,0 +1,21 @@
+
+
+
+def addSum(num1,num2):
+    print(num1+num2)
+
+
+def return_String(a,b):
+   
+   hello =  a+b
+   
+   return hello
+
+yello = return_String("Testify ","Python")
+
+
+addSum(10,190)
+print(yello)
+    
+
+
