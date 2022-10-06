4e06150f03a3e85b224d9fe8d4c3d1516cb49b89
sultanajayi <hayobamidele39@gmail.com>
2022-10-06 21:52:57 +0100

    New task

---
+++ Python Task/task_15.py
@@ -0,0 +1,7 @@
+new_list = ['JavaScript', 'php', 'C#', 'Python', 'Java']
+
+print("list: ", new_list)
+
+new_list.sort()
+
+print('Sorted List: ', new_list)
---
+++ Python Task/task_16.py
@@ -0,0 +1,11 @@
+new_car = {
+    "maker": "Total",
+    "colour": "Black",
+    "car_type": "Camry"
+}
+print(new_car)
+
+new_car.update({"yr": 1998 , "wheels": 4})
+
+print(new_car)
+
